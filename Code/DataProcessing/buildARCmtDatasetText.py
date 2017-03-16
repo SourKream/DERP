60e3a8bf3058154dfef79ad83b9bdaf43bc88893
Shantanu Kumar <kumarshantanu95@gmail.com>
2017-03-16 10:02:59 +0530

    Added pruning and sample data

--- Code/DataProcessing/buildARCmtDatasetText.py
+++ Code/DataProcessing/buildARCmtDatasetText.py
@@ -35,19 +35,43 @@ for line in logProgress(open(data_path + 'UsefulComments_NoChildren.txt'), every
 	if line['name']	in uid_set:
 		body_dict[line['name']] = filterByTab(line['body'])
 
+## Pruning rules
+# 1. URL present in comment or context
+# 2. Comment with length >= 40
+# 3. Comment with length <= 3
+
+def URLPresent(body):
+	if "http" in body or "www" in body:
+		return True
+	return False
+
 ## Dump data to file
 print 'Starting with writing to file ...'
 
 DELIMITER = '<CTX>'
-with open(data_path+'ARCmtDataset_text.txt','w') as f:
+with open(data_path+'ARCmtDataset_text_Pruned.txt','w') as f:
 	for comment in logProgress(idx_data):
 		buf = ""
 		for context_id in comment[0]:
 			if context_id in body_dict:
+				if URLPresent(body_dict[context_id]):
+					continue
 				buf += ' ' + DELIMITER + ' ' + body_dict[context_id]
 		if buf == "": 
 			continue
 		if comment[1] in body_dict and comment[2] in body_dict:
+			if URLPresent(body_dict[comment[1]]) or URLPresent(body_dict[comment[2]]):
+				continue
+			if len(body_dict[comment[1]].split(' ')) >= 40 or len(body_dict[comment[2]].split(' ')) >= 40:
+				continue
+			if len(body_dict[comment[1]].split(' ')) <= 3 or len(body_dict[comment[2]].split(' ')) <= 3:
+				continue
+			# To create only Pos/Neg dataset
+			# if comment[3] == '0\n':
+			# if comment[3] == '1\n':
+			# 	continue
 			buf += '\t' + body_dict[comment[1]] + '\t' + body_dict[comment[2]] + '\t' + comment[3]
+			if len(buf.split('\t')) != 4:
+				continue
 			f.write(buf)
 
