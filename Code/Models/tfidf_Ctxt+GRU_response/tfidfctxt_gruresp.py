633d3067af33eeb37ac080a55d462d6da37cad22
suragnair <suragnair@hotmail.com>
2017-03-23 17:20:51 +0530

    minor updates

--- Code/Models/tfidf_Ctxt+GRU_response/tfidfctxt_gruresp.py
+++ Code/Models/tfidf_Ctxt+GRU_response/tfidfctxt_gruresp.py
@@ -1,9 +1,8 @@
 import keras
-from keras.layers import Input, GRU, Embedding, Dense
+from keras.layers import Input, GRU, Embedding, Dense, Dropout
 from keras.models import Model
 from keras.callbacks import Callback
 from keras.layers.wrappers import Bidirectional
-from nltk.tokenize import TreebankWordTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.externals import joblib
@@ -16,11 +15,12 @@ GRU_HIDDEN_STATE = 300
 VOCAB_SIZE = 50000    # 1 for UNK
 MAX_RESP_LEN = 50
 EMBEDDING_DIM = 300
-DENSE_HIDDEN_STATE = 30
+DENSE_HIDDEN_STATE = 100
 
 # training details
 TRAIN_SIZE = -1    # -1 => train on all
-BATCH_SIZE = 256
+BATCH_SIZE = 512
+DROPOUT = 0.3
 
 # hpc file paths
 train_file = '/scratch/cse/dual/cs5130275/DERP/Reddit/DatasetNewPruned11M/train.txt'
@@ -28,11 +28,12 @@ val_file = '/scratch/cse/dual/cs5130275/DERP/Reddit/DatasetNewPruned11M/val.txt'
 test_file = '/scratch/cse/dual/cs5130275/DERP/Reddit/DatasetNewPruned11M/test.txt'
 count_vect_vocab_file = '/home/cse/dual/cs5130275/DERP/Code/Models/LogisticRegBaseline/vocab_50k'
 tfidf_transformer_file = '/home/cse/dual/cs5130275/DERP/Code/Models/LogisticRegBaseline/tfidf_transformer_50k'
-save_model_path = '/scratch/cse/dual/cs5130275/DERP/Models/tfidf_Ctxt+GRU_response/' + 'GRU_HIDDEN_STATE_' + str(GRU_HIDDEN_STATE) + '_VOCAB_SIZE_' + str(VOCAB_SIZE) + '_MAX_RESP_LEN_' + str(MAX_RESP_LEN) + '_EMBEDDING_DIM_' + str(EMBEDDING_DIM) + '_DENSE_HIDDEN_STATE_' + str(DENSE_HIDDEN_STATE) + '_BATCH_SIZE_' + str(BATCH_SIZE)
+save_model_path = '/scratch/cse/dual/cs5130275/DERP/Models/tfidf_Ctxt+GRU_response/' + 'GRU_HIDDEN_STATE_' + str(GRU_HIDDEN_STATE) + '_VOCAB_SIZE_' + str(VOCAB_SIZE) + '_MAX_RESP_LEN_' + str(MAX_RESP_LEN) + '_EMBEDDING_DIM_' + str(EMBEDDING_DIM) + '_DENSE_HIDDEN_STATE_' + str(DENSE_HIDDEN_STATE) + '_DROPOUT_' + str(DROPOUT) + '_BATCH_SIZE_' + str(BATCH_SIZE)
 load_model_path = ''
 
 # local file paths
 # train_file = 'train100.txt'
+# val_file = 'train100.txt'
 # count_vect_vocab_file = '../LogisticRegBaseline/vocab_50k'
 # tfidf_transformer_file = '../LogisticRegBaseline/tfidf_transformer_50k'
 
@@ -57,14 +58,16 @@ def create_model():
     gold_resp_emb = embedding(gold_resp)
     alt_resp_emb = embedding(alt_resp)
     
-    shared_gru = Bidirectional(GRU(GRU_HIDDEN_STATE))
+    shared_gru = Bidirectional(GRU(GRU_HIDDEN_STATE, dropout=DROPOUT))
     
     encoded_gold_resp = shared_gru(gold_resp_emb)
     encoded_alt_resp = shared_gru(alt_resp_emb)
     
     merged_vector = keras.layers.concatenate([ctxt_tfidf, encoded_gold_resp, encoded_alt_resp], axis=-1)
+    merged_vector = Dropout(DROPOUT)(merged_vector)
     merged_vector = Dense(DENSE_HIDDEN_STATE, activation='tanh')(merged_vector)
-    
+    merged_vector = Dropout(DROPOUT)(merged_vector)
+        
     predictions = Dense(1, activation='sigmoid')(merged_vector)
     
     model = Model(inputs=[ctxt_tfidf, gold_resp, alt_resp], outputs=predictions)
@@ -84,15 +87,15 @@ if __name__=='__main__':
     
     # prepare vocab, count_vect
     assert(len(count_vect_vocab)==VOCAB_SIZE)
-    count_vect = CountVectorizer(tokenizer=my_tokenizeDatasetNewPruned11M)
+    count_vect = CountVectorizer(tokenizer=my_tokenize)
     count_vect.vocabulary_ = count_vect_vocab
     vocab_dict = {x:i+1 for i,x in enumerate(count_vect_vocab)}     # +1 since 0 is for masking
     vocab_dict['UNK'] = len(vocab_dict)+1
     inv_vocab = {vocab_dict[x]:x for x in vocab_dict}
     
     # generators
-    train_gen = data_generator(train_x, train_y, vocab_dict, inv_vocab, count_vect, tfidf_transformer)
-    val_gen = data_generator(val_x, val_y, vocab_dict, inv_vocab, count_vect, tfidf_transformer)
+    train_gen = data_generator(train_x, train_y, vocab_dict, count_vect, tfidf_transformer)
+    val_gen = data_generator(val_x, val_y, vocab_dict, count_vect, tfidf_transformer)
     
     # model/callbacks
     model = create_model()
@@ -100,4 +103,4 @@ if __name__=='__main__':
     weight_save.setModelFile(save_model_path)
     
     # train!
-    model.fit_generator(train_gen, steps_per_epoch=len(train_x)/BATCH_SIZE, epochs=10, validation_data=val_gen, validation_steps=len(val_x)/BATCH_SIZE, callbacks=[weight_save]) 
+    model.fit_generator(train_gen, steps_per_epoch=len(train_x)/BATCH_SIZE/10, epochs=10*10, validation_data=val_gen, validation_steps=len(val_x)/BATCH_SIZE, callbacks=[weight_save]) 
